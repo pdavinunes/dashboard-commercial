f473fd0e50d44836d2d5f6c8e3bf81cc9c725b87
pdavinunes <p.davi.nunes@gmail.com>
2020-07-08 09:43:39 -0300

    CRUD de ProductController

---
+++ backend/src/controllers/ProductController.js
@@ -0,0 +1,70 @@
+const knex = require('../database/connection');
+
+const checkIfExists = async (id, res) => {
+    const product = await knex('products').select('*').where({id}).catch(_ => {
+        return res.status(400).json({"error":{"message":`Formato inválido de requisição: ${id}`}});
+        });
+    return !!product.length;
+}
+
+module.exports = {
+    async index(req, res) {
+        const products = await knex('products').select('*');
+        return res.json({products}); 
+    },
+
+    async show(req, res) {
+        const {id} = req.params;
+        const product = await knex('products').select('*').where({id}).catch(_ => {
+                return res.status(400).json({"error":{"message":`Formato inválido de requisição: ${id}`}});
+            });
+        return !!product.length ? res.json({product}) : 
+        res.status(404).json({"error":{"message":`Produto não encontrado para o id: ${id}.`}});
+    },
+
+    async create(req, res) {
+        const {name, description, price, comments} = req.body;
+        const product = {name, description, price, comments};
+        const trx = await knex.transaction();
+        await trx('products').insert(product)
+                .then((resp) => {
+                    trx.commit();
+                    return res.status(201).json({"product":{...product}})
+                }).catch( e => {
+                    return res.status(400).json({"error":{"message":`O campo '${e.column}' precisa ser preenchido.`}});
+                })
+    },
+
+    async update(req, res) {
+        const {id} = req.params;
+        const product = {...req.body};
+        
+        await checkIfExists(id, res).then(async (resp) => {
+            if(!resp) return res.status(404).json({"error":{"message":`Produto não encontrado para o id: ${id}.`}});
+            
+            const updated_at = new Date();
+            const trx = await knex.transaction();
+            await trx('products').where({id}).update({...product, updated_at}, ['*'])
+                .then((product) => {
+                    trx.commit();
+                    res.json({product})
+                }).catch( _ => {
+                    return res.status(400).json({"error":{"message":"Não foi possivel atualizar, verifique os dados passados."}});
+                })
+        })
+    },
+
+    async delete(req, res) {
+        const {id} = req.params;
+
+        await checkIfExists(id, res).then(async (resp) => {
+            if(!resp) return res.status(404).json({"error":{"message":`Produto não encontrado para o id: ${id}.`}});
+            
+            const trx = await knex.transaction();
+            await trx('products').where({id}).del();
+            await trx.commit();
+
+            res.status(204);
+        })
+    }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,20 @@
 const { Router } = require('express');
 
 const StoreController = require('./controllers/StoreController');
+const ProductController = require('./controllers/ProductController');
 
 const routes = Router();
 
 routes.get('/stores', StoreController.index);
 routes.get('/stores/:id', StoreController.show);
-routes.post('/stores', StoreController.store);
+routes.post('/stores', StoreController.create);
 routes.put('/stores/:id', StoreController.update);
 routes.delete('/stores/:id', StoreController.delete);
+
+routes.get('/products', ProductController.index);
+routes.get('/products/:id', ProductController.show);
+routes.post('/products', ProductController.create);
+routes.put('/products/:id', ProductController.update);
+routes.delete('/products/:id', ProductController.delete);
 
 module.exports = routes;
