eb05e62f5e7cc465f38d8b2b9e212751c013dd54
pdavinunes <p.davi.nunes@gmail.com>
2020-07-08 00:17:22 -0300

    CRUD do controller store criado

---
+++ backend/src/controllers/StoreController.js
@@ -0,0 +1,66 @@
+const knex = require('../database/connection');
+
+const checkIfExists = async (id) => {
+    const store = await knex('stores').select('*').where({id});
+    return !!store.length;
+}
+
+module.exports = {
+    async index(req, res) {
+        const stores = await knex('stores').select('*');
+        return res.json({stores}); 
+    },
+
+    async show(req, res) {
+        const {id} = req.params;
+        const store = await knex('stores').select('*').where({id});
+        return !!store.length ? res.json({store}) : 
+        res.status(404).json({"error":{"message":`Loja não encontrada para o id: ${id}.`}});
+    },
+
+    async store(req, res) {
+        const {name, description, address, city, uf} = req.body;
+        const store = {name, description, address, city, uf};
+        const trx = await knex.transaction();
+        await trx('stores').insert(store)
+                .then((resp) => {
+                    trx.commit();
+                    return res.status(201).json({"store":{...store}})
+                }).catch( e => {
+                    return res.status(400).json({"error":{"message":`O campo '${e.column}' precisa ser preenchido.`}});
+                })
+    },
+
+    async update(req, res) {
+        const {id} = req.params;
+        const store = {...req.body};
+        
+        await checkIfExists(id).then(async (resp) => {
+            if(!resp) return res.status(404).json({"error":{"message":`Loja não encontrada para o id: ${id}.`}});
+            
+            const updated_at = new Date();
+            const trx = await knex.transaction();
+            await trx('stores').where({id}).update({...store, updated_at}, ['*'])
+                .then((store) => {
+                    trx.commit();
+                    res.json({store})
+                }).catch( _ => {
+                    return res.status(400).json({"error":{"message":"Não foi possivel atualizar, verifique os dados passados."}});
+                })
+        })
+    },
+
+    async delete(req, res) {
+        const {id} = req.params;
+
+        await checkIfExists(id).then(async (resp) => {
+            if(!resp) return res.status(404).json({"error":{"message":`Loja não encontrada para o id: ${id}.`}});
+            
+            const trx = await knex.transaction();
+            await trx('stores').where({id}).del();
+            await trx.commit();
+
+            res.status(204);
+        })
+    }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,13 @@
 const { Router } = require('express');
 
+const StoreController = require('./controllers/StoreController');
+
 const routes = Router();
+
+routes.get('/stores', StoreController.index);
+routes.get('/stores/:id', StoreController.show);
+routes.post('/stores', StoreController.store);
+routes.put('/stores/:id', StoreController.update);
+routes.delete('/stores/:id', StoreController.delete);
 
 module.exports = routes;
