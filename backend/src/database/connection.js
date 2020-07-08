f37343619b2dc3be51137638d2f243a991216fae
pdavinunes <p.davi.nunes@gmail.com>
2020-07-07 22:51:57 -0300

    Adicionando conexão com o banco

---
+++ backend/src/database/connection.js
@@ -0,0 +1,15 @@
+const knex = require('knex');
+require('dotenv').config('.env');
+
+const connection = knex({
+    client: process.env.DB_CLIENT,
+    version: process.env.DB_VERSION,
+    connection: {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
+    }
+});
+
+module.exports = connection;
